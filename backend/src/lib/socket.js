ffdc4157ee1b1840c50816d3869cee9511f3d1b9
Nifa1211 <eshanyapadial@gmail.com>
2026-03-06 01:08:48 +0530

    allow vercel url

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: function(origin, callback) {
+      if (!origin || origin.endsWith(".vercel.app") || origin === "http://localhost:5173") {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   },
 });
 
@@ -15,8 +22,7 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,7 +30,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
