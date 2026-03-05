327999534a8322781ac1a025a8598de39a950f25
Nifa1211 <eshanyapadial@gmail.com>
2026-03-06 01:17:40 +0530

    sidebar issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,16 +1,18 @@
 import jwt from 'jsonwebtoken';
 
-
-export const generateToken = (userId,res) => {
+export const generateToken = (userId, res) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: '7d',
-})
+    });
+
+    const isProduction = process.env.NODE_ENV === 'production';
 
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'strict',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        secure: isProduction,
+        sameSite: isProduction ? 'none' : 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
     return token;
 };
